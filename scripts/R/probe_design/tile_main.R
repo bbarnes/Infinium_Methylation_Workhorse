edecb87faffeb3c6159b11a2c623d652f427c0bf
Bret D. Barnes <bbarnes@illumina.com>
2020-07-31 19:09:46 -0700

    Adding...

--- scripts/R/probe_design/tile_main.R
+++ scripts/R/probe_design/tile_main.R
@@ -618,22 +618,15 @@ if (!is.null(imp_out_tsv) & file.exists(imp_out_tsv)) {
     cat(glue::glue("[{par$prgmTag}]: Ready to launch alignments; genomes count={gen_cnt}; gen_path={gen_path}...{RET}"))
     
     shell_dir <- file.path(opt$outDir, 'shells')
-    
-    if (file.exists(gen_path)) {
-      bow_tsv <- bowtieProbeAlign(exe=par$bow_exe, fas=prb_snp_fas, gen=gen_path, dir=shell_dir,
-                                  verbose=opt$verbose,vt=5,tc=1,tt=pTracker)
-      
-      bsp_tsv <- bsmapProbeAlign(exe=par$bsp_exe, fas=prb_cgn_fas, gen=gen_path, dir=shell_dir,
-                                 verbose=opt$verbose,vt=5,tc=1,tt=pTracker)
 
-    } else if (dir.exists(gen_path)) {
-      cat(glue::glue("[{par$prgmTag}]: Searching directory for genome: gen_path={gen_path}...{RET}"))
+    if (dir.exists(gen_path)) {
+      cat(glue::glue("[{par$prgmTag}]:{TAB} Searching directory for genomes: gen_path={gen_path}...{RET}"))
       gen_paths <- list.files(gen_path, pattern='.fa[.gz]$', full.names=TRUE)
       
       gen_cnts <- 0
       for (gpath in gen_paths) {
         if (file.exists(gpath)) {
-          cat(glue::glue("[{par$prgmTag}]:{TAB}Ready to launch alignments; genomes count={gen_cnt}/{gen_cnts}; gen_path={gen_path}...{RET}"))
+          cat(glue::glue("[{par$prgmTag}]:{TAB}{TAB}Ready to launch alignments; genomes count={gen_cnt}/{gen_cnts}; gen_path={gen_path}...{RET}"))
           
           bow_tsv <- bowtieProbeAlign(exe=par$bow_exe, fas=prb_snp_fas, gen=gpath, dir=shell_dir,
                                       verbose=opt$verbose,vt=5,tc=1,tt=pTracker)
@@ -645,7 +638,16 @@ if (!is.null(imp_out_tsv) & file.exists(imp_out_tsv)) {
         gen_cnts <- gen_cnts + 1
         if (gen_cnts > 10) break
       }
+    } else if (file.exists(gen_path)) {
+      cat(glue::glue("[{par$prgmTag}]:{TAB} Using Genome Path={gen_path}...{RET}"))
+      bow_tsv <- bowtieProbeAlign(exe=par$bow_exe, fas=prb_snp_fas, gen=gen_path, dir=shell_dir,
+                                  verbose=opt$verbose,vt=5,tc=1,tt=pTracker)
+      
+      bsp_tsv <- bsmapProbeAlign(exe=par$bsp_exe, fas=prb_cgn_fas, gen=gen_path, dir=shell_dir,
+                                 verbose=opt$verbose,vt=5,tc=1,tt=pTracker)
+
     }
+    
     # gen_name <- gen_path %>% base::basename() %>% stringr::str_remove('.[A-Za-z]+$') %>% stringr::str_remove('.[A-Za-z]+$')
     # 
     # shell_dir <- file.path(opt$outDir, 'shells')
