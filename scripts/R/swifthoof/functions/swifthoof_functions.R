9b19df0769a69bbc8bfdb5c9f0e7eb62ac81e13f
Bret Barnes <bretbarnes@Brets-MacBook-Pro.local>
2020-11-18 06:33:08 -0800

    Add

--- scripts/R/swifthoof/functions/swifthoof_functions.R
+++ scripts/R/swifthoof/functions/swifthoof_functions.R
@@ -504,14 +504,17 @@ sesamizeSingleSample = function(prefix, man, add, ref, opt, workflows,
     # system(t_cmd)
     
     all_call_tib %>% print()
-    all_call_tib <- all_call_tib %>% dplyr::mutate(p_len=stringr::str_length(Probe_ID)) %>%
-      dplyr::filter(p_len>1)
+    all_call_tib <- all_call_tib %>% 
+      dplyr::mutate(p_len=stringr::str_length(Probe_ID)) %>%
+      dplyr::filter(p_len>1) %>% dplyr::arrange(p_len)
     
+    cat(glue::glue("# ----- ----- ----- ----- ----- -----|----- ----- ----- ----- ----- ----- #{RET}{RET}"))
     cat(glue::glue("Beg; Writing; calls_csv={calls_csv}{RET}"))
     all_call_tib %>% print()
     # readr::write_csv(all_call_tib %>% head(), calls_csv)
     readr::write_csv(all_call_tib, calls_csv)
     cat(glue::glue("End; Writing; calls_csv={calls_csv}{RET}{RET}{RET}"))
+    cat(glue::glue("# ----- ----- ----- ----- ----- -----|----- ----- ----- ----- ----- ----- #{RET}{RET}"))
     
     readr::write_csv(ssheet_tib, ssheet_csv)
     readr::write_csv(time_tib, times_csv)
